49a62ef0d28e906d9ffb10336ffcea86873f1d1c
devisetty99 <vdevisetty@citi-us.com>
2020-01-28 21:34:55 -0500

    updating file

--- try.ps1
+++ try.ps1
@@ -1,4 +1,3 @@
-Get-ExecutionPolicy -List
 
 
  param (
@@ -6,17 +5,21 @@ Get-ExecutionPolicy -List
     $param2, 
     #$param3,
     #$param4,
-    $days = '-0'
+    $days = '-1'
 )
 #$iisbackup =  $"IIS.BackupDirectory"
-$WebsiteName = "DRIS$param3$param4"
+#$WebsiteName = "DRIS$param3$param4"
 $Path = "C:\Users\vdevisetty\Desktop\$param1\$param2"
 #$Path = "$param1\$WebsiteName"
 $Daysback = "$days"
 $CurrentDate = Get-Date
 $DatetoDelete = $CurrentDate.AddDays($Daysback)
-Get-ChildItem $Path -Recurse  | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item -Force -Verbose
-
+Get-ChildItem $Path -Recurse  | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item #-Force -Verbose
+#Remove-Item -path "$Path\*" -Recurse  -Force -EA SilentlyContinue -Verbose
+#Remove-Item -path "$Path\*"  -Recurse | Where-Object { $_.LastWriteTime -lt $DatetoDelete } -Force -EA SilentlyContinue -Verbose
+#-path "$Path\*"| Where-Object { $_.LastWriteTime -lt $DatetoDelete } |
+#Get-ExecutionPolicy -List 
+#Get-ChildItem $pathV
 exit 0
 
 
